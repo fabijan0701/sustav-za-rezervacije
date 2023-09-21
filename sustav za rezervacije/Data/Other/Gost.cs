090307261a43d68420ded54667abd30d276fefb2
fabijansaric9 <145705967+fabijansaric9@users.noreply.github.com>
2023-09-21 19:48:27 +0200

    stvoren direktorij Data

---
+++ sustav za rezervacije/Data/Other/Gost.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPI_Restoran.Data.Other
+{
+
+    /// <summary>
+    /// Klasa Gost sadrži sve potrebne podatke o gostima. Za identifikaciju gosta koristimo broj osobne 
+    ///  iskaznice, čime se uvjeravamo da gost koji želi obaviti rezervaciju nije maloljetan.
+    /// </summary>
+    public class Gost
+    {
+        private int brojOsobne;
+        private string ime;
+        private string prezime;
+        private List<string> kontakti;
+        private static List<Gost> gosti;
+
+        /// <summary>
+        /// Zadani konstruktor koji prima tri parametra
+        /// </summary>
+        /// <param name="brojOsobne"></param>
+        /// <param name="ime"></param>
+        /// <param name="prezime"></param>
+        public Gost(int brojOsobne, string ime, string prezime)
+        {
+            this.brojOsobne = brojOsobne;
+            this.ime = ime;
+            this.prezime = prezime;
+            kontakti = new List<string>();
+        }
+
+        public int BrojOsobne { get => brojOsobne; set => brojOsobne = value; }
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+        public List<string> Kontakti { get => kontakti; set => kontakti = value; }
+
+
+        /// <summary>
+        /// Uz pomoć baze podataka vraca popis svih gostiju
+        /// </summary>
+        public static List<Gost> Gosti { get => Connection.SQL_Gosti(); }
+
+        /// <summary>
+        /// Pretrazuje postoji li gost sa trazenim brojem osobne iskaznice, ako postoji vraća njegovu instancu
+        /// </summary>
+        /// <param name="brojOs"></param>
+        /// <returns></returns>
+        public static Gost Trazi(int brojOs)
+        {
+            foreach(Gost g in Gosti)
+            {
+                if (g.BrojOsobne == brojOs)
+                {
+                    return g;
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Vraca bool vrijednost koja ovisi o tome ima li gost rezervaciju u datom terminu
+        /// </summary>
+        /// <param name="pocetak"></param>
+        /// <param name="kraj"></param>
+        /// <returns></returns>
+        public bool UTerminu(DateTime pocetak, DateTime kraj)
+        {
+            foreach (Rezervacija rez in Connection.ŞQL_Rezervacije(pocetak, kraj))
+            {
+                if (rez.Gost.BrojOsobne == BrojOsobne)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    
+        /// <summary>
+        /// Vraca bool vrijednost ovisno o tome je li uneseni format osobne iskaznice tocan
+        /// </summary>
+        /// <param name="broj"></param>
+        /// <returns></returns>
+        public static bool TocanFormatOsobne(int broj)
+        {
+            //if (broj == 0) return false;
+
+            int duzina = (int)Math.Log10(broj) + 1;
+
+            bool b = duzina == 8? true: false;
+            return b;
+        }
+    }
+}
---
+++ sustav za rezervacije/Data/Other/Rezervacija.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPI_Restoran.Data.Other
+{
+
+    /// <summary>
+    /// Klasa rezervacija sadrži podatke o instancama rezervacije i podatke vezano uz rezervacije
+    /// </summary>
+    public class Rezervacija
+    {
+        private int broj;
+        private Gost gost;
+        private Stol stol;
+        private DateTime pocetak;
+        private DateTime kraj;
+        private string kontakt;
+
+        /// <summary>
+        /// Konstruktor koji prima 5 parametara, obicno se koristi kod citanja rezervacija
+        /// </summary>
+        /// <param name="broj"></param>
+        /// <param name="gost"></param>
+        /// <param name="stol"></param>
+        /// <param name="poc"></param>
+        /// <param name="kr"></param>
+        /// <param name="kontakt"></param>
+        public Rezervacija(int broj, Gost gost, Stol stol, DateTime poc, DateTime kr, string kontakt)
+        {
+            this.broj = broj;
+            this.gost = gost;
+            this.stol = stol;
+            this.pocetak = poc;
+            this.kraj = kr;
+            this.kontakt = kontakt;
+        }
+
+        /// <summary>
+        /// Konstruktor koji prima 4 parametra, obicno se koristi kod unosa rezervacija
+        /// </summary>
+        /// <param name="gost"></param>
+        /// <param name="stol"></param>
+        /// <param name="poc"></param>
+        /// <param name="kr"></param>
+        /// <param name="kontakt"></param>
+        public Rezervacija(Gost gost, Stol stol, DateTime poc, DateTime kr, string kontakt)
+        {
+            this.gost = gost;
+            this.stol = stol;
+            this.pocetak = poc;
+            this.kraj = kr;
+            this.kontakt = kontakt;
+        }
+
+
+        public int Broj { get => broj; set => broj = value; }
+        public Gost Gost { get => gost; set => gost = value; }
+        public Stol Stol { get => stol; set => stol = value; }
+        public DateTime Pocetak { get => pocetak; set => pocetak = value; }
+        public DateTime Kraj { get => kraj; set => kraj = value; }
+        public string Kontakt { get => kontakt; set => kontakt = value; }
+
+        /// <summary>
+        /// Vraca instancu rezervacije prema parametrima koje prima, te null vrijednost
+        ///  ako takva rezervacija ne postoji
+        /// </summary>
+        /// <param name="broj"></param>
+        /// <param name="pocetak"></param>
+        /// <param name="kraj"></param>
+        /// <returns></returns>
+        public static Rezervacija Trazi(int broj, DateTime pocetak, DateTime kraj)
+        {
+            foreach (Rezervacija r in Connection.ŞQL_Rezervacije(pocetak, kraj))
+            {
+                if (r.broj == broj) return r;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// /// Vraca instancu rezervacije prema parametrima koje prima, te null vrijednost
+        ///  ako takva rezervacija ne postoji
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="pocetak"></param>
+        /// <param name="kraj"></param>
+        /// <returns></returns>
+        public static Rezervacija Trazi(Gost g, DateTime pocetak, DateTime kraj)
+        {
+            foreach (Rezervacija r in Connection.ŞQL_Rezervacije(pocetak, kraj))
+            {
+                if (r.Gost.BrojOsobne == g.BrojOsobne) return r;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Vraca string koji ispisuje instancu klase rezervacija
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string br = "Broj: " + Broj;
+
+            string gost = String.Format("Gost: {0} {1}\nBroj dokumenta:{2}",
+                Gost.Ime, Gost.Prezime, Gost.BrojOsobne);
+
+            string stol = String.Format("Stol {0} {1} {2}", Stol.Broj, Stol.Pozicija, Stol.Klasa);
+
+            string datumi = "Pocetak: " + Pocetak.ToString() + "\nKraj: " + Kraj.ToString();
+
+            return br + "\n" + gost + "\n" + stol + "\n" + datumi;
+        }
+    }
+}
