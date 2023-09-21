8956251a7815feb5b797e607da6d01d01eab4ef8
Fabijan Šarić <fabijansaricc@gmail.com>
2023-09-21 17:47:45 +0200

    napravljeno korisničko sučelje forme za unos

---
+++ sustav za rezervacije/Forms/ReservationDataForm.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace sustav_za_rezervacije.Forms
+{
+    partial class ReservationDataForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBrojOsobne = new System.Windows.Forms.TextBox();
+            this.btnOdustani = new System.Windows.Forms.Button();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKontakt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstStol = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(350, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 20);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Broj osobne";
+            // 
+            // txtBrojOsobne
+            // 
+            this.txtBrojOsobne.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtBrojOsobne.Location = new System.Drawing.Point(486, 79);
+            this.txtBrojOsobne.Name = "txtBrojOsobne";
+            this.txtBrojOsobne.Size = new System.Drawing.Size(198, 27);
+            this.txtBrojOsobne.TabIndex = 22;
+            // 
+            // btnOdustani
+            // 
+            this.btnOdustani.Location = new System.Drawing.Point(564, 336);
+            this.btnOdustani.Name = "btnOdustani";
+            this.btnOdustani.Size = new System.Drawing.Size(132, 48);
+            this.btnOdustani.TabIndex = 21;
+            this.btnOdustani.Text = "Odustani";
+            this.btnOdustani.UseVisualStyleBackColor = true;
+            // 
+            // btnSpremi
+            // 
+            this.btnSpremi.Location = new System.Drawing.Point(416, 336);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(132, 48);
+            this.btnSpremi.TabIndex = 20;
+            this.btnSpremi.Text = "Spremi";
+            this.btnSpremi.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(378, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 20);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Kontakt";
+            // 
+            // txtKontakt
+            // 
+            this.txtKontakt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtKontakt.Location = new System.Drawing.Point(486, 229);
+            this.txtKontakt.Name = "txtKontakt";
+            this.txtKontakt.Size = new System.Drawing.Size(198, 27);
+            this.txtKontakt.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(378, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Prezime";
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtPrezime.Location = new System.Drawing.Point(486, 176);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(198, 27);
+            this.txtPrezime.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(404, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 20);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Ime";
+            // 
+            // txtIme
+            // 
+            this.txtIme.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtIme.Location = new System.Drawing.Point(486, 125);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(198, 27);
+            this.txtIme.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(52, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 22);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Dostupni stolovi";
+            // 
+            // lstStol
+            // 
+            this.lstStol.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lstStol.FormattingEnabled = true;
+            this.lstStol.ItemHeight = 22;
+            this.lstStol.Location = new System.Drawing.Point(56, 68);
+            this.lstStol.Name = "lstStol";
+            this.lstStol.Size = new System.Drawing.Size(253, 334);
+            this.lstStol.TabIndex = 12;
+            // 
+            // ReservationDataForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtBrojOsobne);
+            this.Controls.Add(this.btnOdustani);
+            this.Controls.Add(this.btnSpremi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtKontakt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstStol);
+            this.Name = "ReservationDataForm";
+            this.Text = "Unos podataka";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBrojOsobne;
+        private System.Windows.Forms.Button btnOdustani;
+        private System.Windows.Forms.Button btnSpremi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtKontakt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstStol;
+    }
+}
